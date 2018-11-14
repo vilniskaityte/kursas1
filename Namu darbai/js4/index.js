0a649319d1457945fd965b1a753d259210c25020
Julija <vilniskaityte@gmail.com>
2018-11-14 17:30:14 +0200

    pridetas namu darbas js4

---
+++ Namu darbai/js4/index.js
@@ -0,0 +1,11 @@
+var age = prompt("Įveskite amžių", "");
+ if(isNaN(age)){
+
+ }
+if(age < 18){
+    alert("Įeiti į svetainę draudžiama!");
+   }
+
+  if(age >= 18){
+   alert("Į svetainę įeiti galite");
+ }
