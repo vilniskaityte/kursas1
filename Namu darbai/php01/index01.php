cb559e876d66e03af124222376238b8c6b853802
Julija <vilniskaityte@gmail.com>
2018-11-26 21:09:16 +0200

    pridetas namu darbas php01

---
+++ Namu darbai/php01/index01.php
@@ -0,0 +1,20 @@
+<?php
+  $šalis = 'Lietuva';
+  $miestas = 'Vilnius';
+  $populiacija = 574221 ;
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+      <li><?php echo $šalis; ?></li>
+      <li><?php echo $miestas; ?></li>
+      <li><?php echo $populiacija; ?></li>
+    </ul>
+  </body>
+</html>
---
+++ Namu darbai/php01/index02.php
@@ -0,0 +1,16 @@
+<?php
+    $šalis = 'Lietuva';
+    $miestas = 'Vilnius';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p><?php echo $miestas .' - Lietuvos sostinė'; ?></p>
+    <p><?php echo "$miestas ir $šalis sparčiai auga"; ?></p>
+  </body>
+</html>
