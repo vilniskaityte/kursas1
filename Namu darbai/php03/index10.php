4675acde00b91e90fea1ad691acd4cf9bb78543e
Julija <vilniskaityte@gmail.com>
2018-11-28 21:14:16 +0200

    pridetas namu darbas

---
+++ Namu darbai/php03/index10.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      function daugyba($a, $b) {
+        $plotas = $a * $b;
+        echo "Stačiakampio plotas: $plotas";
+      }
+        daugyba(5,10);
+     ?>
+  </body>
+</html>
