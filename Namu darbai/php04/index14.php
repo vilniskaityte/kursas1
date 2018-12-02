a81d72664740e2f9b1657096aaebcc441b6bc4db
Julija <vilniskaityte@gmail.com>
2018-12-02 16:00:23 +0200

    pataisytas namu darbas

--- Namu darbai/php04/index14.php
+++ Namu darbai/php04/index14.php
@@ -1,17 +1,18 @@
 <?php
+
+function dabartiniaiMetai() {
+
     $startYear = 2015;
     $currentYear = date('Y');
 
     if ($startYear < $currentYear) {
       echo '&copy;' . ' ' . $startYear . '-' . $currentYear;
     }
-
-    elseif ($startYear >= $currentYear) {
-      echo '&copy;' . ' ' . $currentYear;
+      else {
+        echo '&copy;' . ' '. $currentYear;
     }
+}
+echo dabartiniaiMetai();
 
-    else {
-      echo '&copy;' . ' '. $currentYear;
-    }
 
 ?>
