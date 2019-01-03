05f6976d61cf34bc1739992b7fe9850cb566e263
Julija <vilniskaityte@gmail.com>
2019-01-03 19:55:26 +0200

    prideti papildomi namu darbai

---
+++ namu darbai po kursu/js1/index.js
@@ -0,0 +1,9 @@
+var colorTitle = "red";
+var colorParagraph = "blue";
+var title = "Skyrius1";
+var article = "Magna ita offendit, anim quibusdam philosophari, an quid praetermissum nam nisi despicationes nescius elit commodo. Ita et distinguantur de id pariatur id laborum, ubi enim probant proident ad duis aut pariatur, sunt iudicem	concursionibus nam fore ab voluptate do nulla consectetur voluptate elit	aliquip, legam ad admodum et quid. Quae probant an commodo, litteris de summis ullamco. Est legam nostrud arbitrantur ex si a culpa fore quorum est an amet quis est aliquip, enim incididunt quo praetermissum. Eu pariatur ab fabulas. Minim nescius cohaerescant, qui quorum eram de singulis.";
+
+
+
+
+  document.write( '<h1 style="color:' + colorTitle + ';">' + title + '</h1>' + '<p style="color:' + colorParagraph + ';">' + article + '</p>'  + '<p>Tekste yra ' + article.length + ' simbolių</p>')
